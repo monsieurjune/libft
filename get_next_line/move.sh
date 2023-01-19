a76e953fdd1f65151c4158d5bec8d658bf2d34b8
Thunpisis Ponuthai <tponutha@z3t3c4.42bangkok.com>
2023-01-20 00:22:43 +0700

    Remove gnl test

--- get_next_line/move.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -f ~/Send/gnl/*.c ~/Send/gnl/*.h
-cp -R ~/Cadet/get_next_line/ ~/Send/gnl/srcs
-mv ~/Send/gnl/srcs/* ~/Send/gnl/
-rm -rf ~/Send/gnl/srcs ~/Send/gnl/move.sh
-rm -rf ~/Send/gnl/gnl1 ~/Send/gnl/gnl2
