89b75bba804cf25e024f3bc69247fb2cc02e5289
Thunpisis Ponuthai <tponutha@z2t18c4.42bangkok.com>
2023-02-22 15:55:43 +0700

    Create Libft

--- libft/srcs/move.sh
+++
@@ -1,4 +0,0 @@
-rm -f ~/Send/libft/*.c ~/Send/libft/*.h ~/Send/libft/Makefile
-cp -R ~/Cadet/libft/srcs ~/Send/libft/srcs
-mv ~/Send/libft/srcs/* ~/Send/libft/
-rm -rf ~/Send/libft/srcs ~/Send/libft/move.sh
